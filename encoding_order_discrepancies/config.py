f2f24e1e759ca5aeb4d49bbe59d6010a5250ccc1
Sudarshan Naidu Dondeti <sudarshan.naidu@sts.in>
2020-03-13 14:54:20 +0530

    Create config.py

---
+++ encoding_order_discrepancies/config.py
@@ -0,0 +1,14 @@
+SNOWFLAKE_PRIVATE_KEY_PATH = path
+SNOWFLAKE_KEY_PASSPHRASE = password
+
+#Snowflake login detals
+user='dparmar',
+account='orchard',
+private_key=private_key,
+database='FACTS',
+schema='PROD',
+warehouse='DEV_OWS_WAREHOUSE',
+role='DEV_ENGINEERING'
+
+#slack channel name
+channel":"automation-script"
