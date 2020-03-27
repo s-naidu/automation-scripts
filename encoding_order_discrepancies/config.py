39aa3c9e9f9161379d254a020d4423c291167d28
Sudarshan Naidu Dondeti <sudarshan.naidu@sts.in>
2020-03-27 19:05:51 +0530

    Update config.py

--- encoding_order_discrepancies/config.py
+++ encoding_order_discrepancies/config.py
@@ -2,13 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv('.env')
-SNOWFLAKE_PASSPHRASE = os.environ.get('SNOWFLAKE_PASSPHRASE')
 SNOWFLAKE_USER_NAME = os.environ.get('SNOWFLAKE_USER_NAME')
 SNOWFLAKE_ACCOUNT = os.environ.get('SNOWFLAKE_ACCOUNT')
 SNOWFLAKE_DATABASE = os.environ.get('SNOWFLAKE_DATABASE')
-SNOWFLAKE_SCHEMA = os.environ.get('SNOWFLAKE_SCHEMA')
-SNOWFLAKE_WAREHOUSE = os.environ.get('SNOWFLAKE_WAREHOUSE')
-SNOWFLAKE_ROLE = os.environ.get('SNOWFLAKE_ROLE')
+SNOWFLAKE_PASSWORD = os.environ.get('SNOWFLAKE_PASSWORD')
 
 SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL')
 SLACK_CHANNEL_NAME = os.environ.get('SLACK_CHANNEL_NAME')
