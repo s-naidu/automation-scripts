c608a232ec81254086bdabab487c7ea82b169ba1
Sudarshan Naidu Dondeti <sudarshan.naidu@sts.in>
2020-03-27 19:02:49 +0530

    Update slack-notification.py

--- encoding_order_discrepancies/slack-notification.py
+++ encoding_order_discrepancies/slack-notification.py
@@ -1,25 +1,19 @@
 from datetime import timedelta, date
+from snowflake.connector import DictCursor
 import requests
 import json
-import pandas as pd
 import os
 import config
 import connection
 
-def get_file():
-    cd = os.getcwd()
-    files = os.listdir(cd)
-    path = ''
-    for file in files:
-        if (file.lower().endswith('.p8')):
-            path = file
-            break
-    return path
-
 def query(yesterday, today, db_connection):
-    db_data = pd.read_sql_query(f"""SELECT encoding_order.entry_date, xy.* 
+    """
+     Query the database to find discrepancies
+    """
+    cursor = db_connection.cursor(DictCursor)
+    query = """SELECT encoding_order.entry_date, xy.* 
     FROM (
-        SELECT encoding_order.encoding_order_id,
+        SELECT encoding_order.encoding_order_id, 
             COUNT(DISTINCT encoding_order_detail.upc) AS eo_upcs_count, 
             COUNT(DISTINCT encoding_queue_detail.upc) AS eq_upcs_count
         FROM orchard_app_reporting.art_relations.encoding_order
@@ -30,45 +24,42 @@ def query(yesterday, today, db_connection):
         INNER JOIN orchard_app_reporting.direct_delivery.encoding_queue_detail
             ON encoding_queue_detail.encoding_queue_id = encoding_queue.encoding_queue_id
         WHERE encoding_order.order_status = 'close' AND encoding_order.processed = 'Y'
-            AND encoding_order.entry_date BETWEEN '{yesterday}' AND '{today}'
+            AND encoding_order.entry_date BETWEEN '{}' AND '{}'
         GROUP BY encoding_order.encoding_order_id
     ) AS xy
     INNER JOIN orchard_app_reporting.art_relations.encoding_order 
         ON encoding_order.encoding_order_id = xy.encoding_order_id
     WHERE xy.eo_upcs_count <> xy.eq_upcs_count
-    ORDER BY 2 """, db_connection)
-    message(db_data)
+    ORDER BY 2 """.format(yesterday, today)
+    cursor.execute(query)
+    send_message(cursor)
 
-def message(db_data):
-    for index, row in db_data.iterrows():
-        i = row['ENCODING_ORDER_ID']
-        eo_count = row['EO_UPCS_COUNT']
-        eq_count = row['EQ_UPCS_COUNT']
-        web_hook = config.SLACK_WEBHOOK_URL
+def send_message(cursor):
+    """
+     Send message to slack if any discrepancy is found
+    """
+    msg = ''
+    for row in cursor:
+        msg = 'Info : {encoding_order_detail_total : ' + str(row['EO_UPCS_COUNT']) + ', encoding_queue_detail_total : ' + str(row['EQ_UPCS_COUNT']) + '}' 
         slack_msg = {
-            'username': '#distro-incomplete-encoding-orders', 
-            'channel': config.SLACK_CHANNEL_NAME, 
-            'attachments': [
+            'username' : '#distro-incomplete-encoding-orders', 
+            'channel' : config.SLACK_CHANNEL_NAME, 
+            'attachments' : [
                 {
-                    'fallback': 'Encoding Order Discrepancies found', 
-                    'color': 'danger', 
-                    'fields': [
-                        {
-                            'title': 'Discrepencies in encoding order ID - ' + str(i), 
-                            'value': 'Info: {encoding_order_detail_total: ' + str(eo_count) + 
-                                     ', encoding_queue_detail_total: ' + str(eq_count) + ' }'
-                        }
-                    ]
+                    'fallback' : 'Encoding Order Discrepancies found', 
+                    'color' : '#DC143C', 
+                    'title' : 'Discrepencies in encoding order ID - {}'.format(row['ENCODING_ORDER_ID']), 
+                    'text' : msg
                 }
-            ]
+            ]    
         }
-        requests.post(web_hook, data=json.dumps(slack_msg))
+        requests.post(config.SLACK_WEBHOOK_URL, data=json.dumps(slack_msg))
+    
 
 def main():
     today = date.today()
     yesterday = (today - timedelta(1))
-    path = get_file() 
-    db_connection = connection.connection(path)
+    db_connection = connection.snowflake_connection()
     query(yesterday, today, db_connection)
 
 if __name__ == '__main__':
