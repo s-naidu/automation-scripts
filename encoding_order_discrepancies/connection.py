6b6ac1ec9d4ad435d875162341057b3581ab95a1
Sudarshan Naidu Dondeti <sudarshan.naidu@sts.in>
2020-03-30 16:32:55 +0530

    Update connection.py

--- encoding_order_discrepancies/connection.py
+++ encoding_order_discrepancies/connection.py
@@ -1,11 +1,12 @@
-import snowflake.connector 
+import snowflake.connector
 import config
 
+
 def snowflake_connection():
-    db_connection = snowflake.connector.connect( 
-        user=config.SNOWFLAKE_USER_NAME, 
-        password=config.SNOWFLAKE_PASSWORD, 
-        account=config.SNOWFLAKE_ACCOUNT,  
+    db_connection = snowflake.connector.connect(
+        user=config.SNOWFLAKE_USER_NAME,
+        password=config.SNOWFLAKE_PASSWORD,
+        account=config.SNOWFLAKE_ACCOUNT,
         database=config.SNOWFLAKE_DATABASE
     )
     return db_connection
