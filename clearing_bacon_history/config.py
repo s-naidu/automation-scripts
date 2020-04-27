73167063aa75e0ab0f895e90046575461e38a78a
Sudarshan Naidu Dondeti <sudarshan.naidu@sts.in>
2020-04-27 17:30:43 +0530

    Update config.py

--- clearing_bacon_history/config.py
+++ clearing_bacon_history/config.py
@@ -1,9 +1,6 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv('.env')
 DATABASE_HOSTNAME = os.environ.get('DATABASE_HOSTNAME')
 DATABASE_USER_NAME = os.environ.get('DATABASE_USER_NAME')
 DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 DATABASE = os.environ.get('DATABASE')
-TICKET_NO = os.environ.get('TICKET_NO')
